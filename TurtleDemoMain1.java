636a4beb7b71528738d1e2f4ab995294ef9251f9
201311245(객체지향프로그래밍) <tkfkdof12@gmail.com>
2016-10-16 16:48:12 +0900

    Create TurtleDemoMain1.java

---
+++ TurtleDemoMain1.java
@@ -0,0 +1,16 @@
+package com.sd.turtle;
+
+import ch.aplu.turtle.*;
+
+class TurtleDemoMain1{
+  TurtleDemoMain1(){
+    Turtle t1=new Turtle();
+    for(int i=0;1<4;i++){
+      t1.forward(50);
+      t1.left(90);
+      }
+    }
+  public static void main(String[] args){
+    new TurtleDemoMain1();
+  }
+}
